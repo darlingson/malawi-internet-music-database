236fccb568a94ce592b0beb29bf11fa004d4ecaa
darlingson <dmakuwila@gmail.com>
2024-02-24 09:06:23 +0200

    created migrations

---
+++ database/migrations/2024_02_24_064942_create_songs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('artist');
+            $table->string('artist_id');
+            $table->string('release_id')->nullable();
+            $table->string('release_name');
+            $table->string('release_type');
+            $table->string('release_day');
+            $table->string('genre');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('songs');
+    }
+};
