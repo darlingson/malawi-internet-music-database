3ac65f073d42fb4e0cbdfef9bf3e1e9c1ea26c39
darlingson <dmakuwila@gmail.com>
2024-03-06 22:18:29 +0200

    added code for the SongController

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Song;
 use Illuminate\Http\Request;
 
 class SongController extends Controller
 {
-    //
+    public function index()
+{
+    return view('songs.index');
+}
+
+public function search(Request $request)
+{
+    $query = $request->input('query');
+
+    $songs = Song::where('title', 'like', "%$query%")
+                  ->orWhere('artist', 'like', "%$query%")
+                  ->get();
+
+    return response()->json($songs);
+}
 }
